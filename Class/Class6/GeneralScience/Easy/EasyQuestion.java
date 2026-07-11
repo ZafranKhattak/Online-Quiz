59a14237a1cf97e8bddb5a2fe64471ddfabba360
ZafranKhattak <zafrankhansoftware.engineer@gmail.com>
2026-07-11 22:39:06 +0500

    Third Commit

--- Class/Class6/GeneralScience/Easy/EasyQuestion.java
+++ Class/Class6/GeneralScience/Easy/EasyQuestion.java
@@ -13,10 +13,10 @@ public class EasyQuestion {
         frame.setLayout(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-         // ================= JPANEL ======================//
+        // ================= JPANEL ======================//
         JPanel panel = new JPanel();
         panel.setLayout(null);
-        panel.setBounds(0,0,1300, 700);
+        panel.setBounds(0, 0, 1300, 700);
         panel.setBackground(Color.BLACK);
         panel.setPreferredSize(new Dimension(450, 1500)); // Height zyada
         frame.add(panel);
@@ -24,22 +24,65 @@ public class EasyQuestion {
         // ================= NOTES FOR QUIZ ================== //
         JLabel label = new JLabel();
         label.setLayout(null);
-        label.setText("<html>There are four options.Select the correct one<br>Take your time.Best of Luck</br></html");     
-        label.setBounds(450 , 40 , 550 , 60);
+        label.setText("<html>There are four options.Select the correct one<br>Take your time.Best of Luck.</br></html");
+        label.setBounds(450, 40, 550, 60);
         label.setForeground(Color.WHITE);
-        label.setFont(new Font("Arial" , Font.ITALIC, 18));
+        label.setFont(new Font("Arial", Font.ITALIC, 18));
         panel.add(label);
 
-        // ======================== JScrollPane ===================//
-        JScrollPane scroll = new JScrollPane(panel);
-        scroll.setBounds(0,0, 1300,400);
-         scroll.setVerticalScrollBarPolicy(
-                JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        // ==================QUESTIONS LOGIC ==================;
 
-        scroll.getVerticalScrollBar().setUnitIncrement(20);
-         frame.add(scroll);
+        JLabel label1= labelCreate("Question 1: What is the basic unit of all living things?",
+                                130
+                                 );
+        panel.add(label1);
         
+        JRadioButton[] options = btn(
+                        "Option A",
+                        "Option B",
+                        "Option C",
+                        "Option D"
+);
+
+    ButtonGroup grp = new ButtonGroup();
+
+for (JRadioButton option : options) {
+    panel.add(option);
+    grp.add(option);
+}
+                        
     }
 
-}
+    public JLabel labelCreate(String question , int y)
+    {
+        JLabel label = new JLabel();
+        label.setText(question);
+        label.setLayout(null);
+        label.setForeground(Color.WHITE);
+        label.setBounds(10 , y, 650 , 30);
+        label.setFont(new Font("Arial" , Font.BOLD,19));
+        return label;
+    }
 
+    public JRadioButton[] btn(String optionA,
+                          String optionB,
+                          String optionC,
+                          String optionD
+                          ) {
+
+    JRadioButton[] btn = new JRadioButton[4];
+
+    btn[0] = new JRadioButton(optionA);
+    btn[1] = new JRadioButton(optionB);
+    btn[2] = new JRadioButton(optionC);
+    btn[3] = new JRadioButton(optionD);
+
+    for (int i = 0; i < btn.length; i++) {
+
+        btn[i].setBounds(200*i,170 , 150, 30);
+        btn[i].setFont(new Font("Arial", Font.BOLD, 13));
+    }
+
+    return btn;
+}
+}
